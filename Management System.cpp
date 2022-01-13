e7d480bf12f823ac88786b40b7890491282dc97e
Wilson <93686724+Wilson9098@users.noreply.github.com>
2022-01-13 21:59:03 +1300

    Add files via upload

---
+++ Management System.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#include "Management System.h"
+
+
+void System::showMenu()
+{
+	cout << "*************************************" << endl;
+	cout << "******** Welcome  to  System!   *****" << endl;
+	cout << "********* 1.Add Information *********" << endl;
+	cout << "********* 2.Display Information *****" << endl;
+	cout << "********* 3.Delete Information ******" << endl;
+	cout << "********* 4.Modify Information ******" << endl;
+	cout << "********* 5.Search Information ******" << endl;
+	cout << "********* 6.Sort Information ********" << endl;
+	cout << "********* 7.Empty Information *******" << endl;
+	cout << "********* 0.    Exit     ************" << endl;
+	cout << "*************************************" << endl;
+}
