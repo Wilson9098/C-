eed2398389fb9f6198770b2dc9de505c63898958
Wilson <93686724+Wilson9098@users.noreply.github.com>
2022-01-15 18:12:34 +1300

    Add files via upload

--- Management System.cpp
+++ Management System.cpp
@@ -152,8 +152,10 @@ void System::Add()
 		cout << "Try Again" << endl;
 	}
 	
-	system("cls");
+	cout << "Added " << addNum << " Person" << endl;
 
+	system("pause");
+	system("cls");
 
 }
 
@@ -180,4 +182,326 @@ void System::Display()
 
 	system("pause");
 	system("cls");
+}
+
+
+bool System::Search(int id)
+{
+	/*if (!Arrsize)
+	{
+		cout << "No Data" << endl;
+		return;
+	}*/
+
+	/*int id;
+	cout << "Please Input an ID" << endl;
+	cin >> id;*/
+
+	/*int count = 0;*/
+	bool ret = false;
+	for (int i = 0; i < Arrsize; i++)
+	{
+		if (arr[i]->m_ID == id)
+		{
+			/*cout << "Name: " << arr[i]->m_Name << endl;
+			cout << "ID: " << arr[i]->m_ID << endl;
+			cout <<"Department: " << arr[i]->m_DeptNO << endl;*/
+			arr[i]->showInfo();
+			cout << endl;
+			/*count++;*/
+			ret = true;
+		}
+	}
+
+	if (!ret)
+		cout << "Can't Find It" << endl;
+
+	system("pause");
+	system("cls");
+	return ret;
+}
+
+bool System::Search(string name)
+{
+	/*if (!Arrsize)
+	{
+		cout << "No Data" << endl;
+		return;
+	}*/
+
+	/*string name;
+	cout << "Please Input a Name" << endl;
+	cin >> name;*/
+
+	/*int count = 0;*/
+	bool ret = false;
+	for (int i = 0; i < Arrsize; i++)
+	{
+		if (arr[i]->m_Name == name)
+		{
+			/*cout << "Name: " << arr[i]->m_Name << endl;
+			cout << "ID: " << arr[i]->m_ID << endl;
+			cout << "Department: " << arr[i]->m_DeptNO << endl;*/
+			arr[i]->showInfo();
+			cout << endl;
+			/*count++;*/
+			ret = true;
+		}
+	}
+
+	if (!ret)
+		cout << "Can't Find It" << endl;
+
+	system("pause");
+	system("cls");
+	return ret;
+}
+
+bool System::Search()
+{
+	if (!Arrsize)
+	{
+		cout << "No Data" << endl;
+		return false;
+	}
+
+	cout << "Search by:" << endl;
+	cout << "1. ID" << endl;
+	cout << "2. Name" << endl;
+	int select = 0;
+	do
+	{
+		cin >> select;
+	} while (select < 1 || select > 2);
+
+	if (select == 1)
+	{
+		int id;
+		cout << "Please Input an ID" << endl;
+		cin >> id;
+		return Search(id);
+	}
+
+	if (select == 2)
+	{
+		string name;
+		cout << "Please Input a Name" << endl;
+		cin >> name;
+		return Search(name);
+	}
+
+}
+
+
+int System::Exit(int id)
+{
+	for (int i = 0; i < Arrsize; i++)
+	{
+		if (arr[i]->m_ID == id)
+		{
+			arr[i]->showInfo();
+			cout << endl;
+			return i;
+		}
+	}
+
+	return -1;
+}
+
+void System::Delete()
+{
+	if (!Arrsize)
+	{
+		cout << "No Data" << endl;
+		return;
+	}
+
+	int id;
+	cout << "Please Input an ID" << endl;
+	cin >> id;
+
+	int i = Exit(id);
+	if (i >= 0)
+	{
+		int select = 0;
+		do 
+		{
+			cout << "Do You Want To Delete It?" << endl;
+			cout << "1. Yes" << endl;
+			cout << "2. No" << endl;
+			cin >> select;
+		} while (select < 1 || select > 2);
+
+		if (select == 1)
+		{
+			i++;
+			for (; i < Arrsize; i++)
+			{
+				arr[i - 1] = arr[i];
+			}
+
+			Arrsize--;
+			cout << "Deleted" << endl;
+		}
+	}
+	else
+	{
+		cout << "Can't Find It" << endl;
+	}
+
+	system("pause");
+	system("cls");
+}
+
+
+void System::Modify()
+{
+	if (!Arrsize)
+	{
+		cout << "No Data" << endl;
+		return;
+	}
+
+	int id;
+	cout << "Please Input an ID" << endl;
+	cin >> id;
+
+	int i = Exit(id);
+	if (i >= 0)
+	{
+		cout << "Please Input a Name" << endl;
+		cin >> arr[i]->m_Name;
+		cout << "Please Input an ID" << endl;
+		cin >> arr[i]->m_ID;
+
+		int dept;
+		do
+		{
+			cout << "Please Select a Department" << endl;
+			cout << "1. Staff" << endl;
+			cout << "2. Manager" << endl;
+			cout << "3. Boss" << endl;
+			cin >> dept;
+		} while (dept < 1 || dept > 3);
+		
+		arr[i]->m_DeptNO = dept;
+		cout << "Modified" << endl;
+	}
+	else
+	{
+		cout << "Can't Find It" << endl;
+	}
+
+	system("pause");
+	system("cls");
+}
+
+
+void System::Sort()
+{
+	if (!Arrsize)
+	{
+		cout << "No Data" << endl;
+		return;
+	}
+
+	int select = 0;
+	do
+	{
+		cout << "Sort by:" << endl;
+		cout << "1. ID" << endl;
+		cout << "2. Name" << endl;
+		cin >> select;
+	} while (select < 1 || select > 2);
+
+	if (select == 1)
+	{
+		SortbyID(arr,0,Arrsize-1);
+	}
+
+	/*if (select == 2)
+	{
+		SortbyName();
+	}*/
+
+	cout << "Sorted" << endl;
+	system("pause");
+	system("cls");
+}
+
+void System::Swap(Person** arr, int a, int b)
+{
+	assert(arr);
+
+	Person* tmp = arr[a];
+	arr[a] = arr[b];
+	arr[b] = tmp;
+}
+
+int System::ComparebyID(Person* a, Person* b)
+{
+	assert(a && b);
+
+	return a->m_ID - b->m_ID;
+}
+
+
+void System::PatitionbyID(Person** arr, const int L, const int R, int* ret)
+{
+	assert(arr && ret);
+	if (L >= R)
+		return;
+
+	int pL = L - 1;
+	int pR = R;
+	int i = L;
+
+	while (i < pR)
+	{
+		if (ComparebyID(arr[i], arr[R])<0)
+		{
+			Swap(arr, i++, ++pL);
+		}
+		else if (ComparebyID(arr[i], arr[R]) > 0)
+		{
+			Swap(arr, i, --pR);
+		}
+		else
+		{
+			i++;
+		}
+	}
+	Swap(arr, R, pR++);
+	ret[0] = pL;
+	ret[1] = pR;
+}
+
+//void System::QuickSort(Person** arr, const int L, const int R)
+//{
+//	assert(arr);
+//	if (L >= R)
+//		return;
+//
+//	srand((unsigned)time(NULL));
+//	Swap(arr, R, L + rand() % (R - L + 1));
+//	int ret[2] = { 0 };
+//
+//	Patition(arr, L, R, ret);
+//	QuickSort(arr, L, ret[0]);
+//	QuickSort(arr, ret[1], R);
+//}
+
+void System::SortbyID(Person** arr, const int L, const int R)
+{
+	assert(arr);
+	if (L >= R)
+		return;
+
+	srand((unsigned)time(NULL));
+	Swap(arr, R, L + rand() % (R - L + 1));
+	int ret[2] = { 0 };
+
+	PatitionbyID(arr, L, R, ret);
+	SortbyID(arr, L, ret[0]);
+	SortbyID(arr, ret[1], R);
 }
