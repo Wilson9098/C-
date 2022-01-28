25e2fd2d2d70be06c603ae4c8430175bcd7fb337
Wilson <93686724+Wilson9098@users.noreply.github.com>
2022-01-28 16:10:54 +1300

    Add files via upload

--- 8th practice.cpp
+++ 8th practice.cpp
@@ -5,6 +5,8 @@ using namespace std;
 #include <string>
 #include <vector>
 #include <map>
+#include <functional>
+#include <algorithm>
 
 #define SALES 0
 #define MARKETING 1
@@ -94,10 +96,37 @@ void test()
 	showbyDept(MANAGER, m);
 }
 
+void test1()
+{
+	vector<bool> v;
+	v.push_back(true);
+	v.push_back(false);
+	v.push_back(true);
+	v.push_back(false);
+
+	for (vector<bool>::iterator it = v.begin(); it != v.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+
+	vector<bool> v1;
+	v1.resize(4);
+
+	transform(v.begin(), v.end(), v1.begin(), logical_not<bool>());
+
+	for (vector<bool>::iterator it = v1.begin(); it != v1.end(); it++)
+	{
+		cout << *it << " ";
+	}
+	cout << endl;
+}
+
 int main()
 {
 	srand((unsigned)time(NULL));
-	test();
+	/*test();*/
+	test1();
 
 	return 0;
 }
