76386da32127ab0afd213f18dc56ba0365bc502b
Wilson <93686724+Wilson9098@users.noreply.github.com>
2022-02-04 14:38:52 +1300

    Add files via upload

--- 9th practice.cpp
+++ 9th practice.cpp
@@ -193,10 +193,39 @@ void test01()
 
 }
 
+void test02()
+{
+	vector<int> v1;
+	vector<int> v2;
+
+	for (int i = 0; i < 10; i++)
+	{
+		v1.push_back(i);
+		v2.push_back(i + 5);
+	}
+
+	vector<int> v3;
+	v3.resize(min(v1.size(), v2.size()));
+	vector<int>::iterator itEnd = set_intersection(v1.begin(), v1.end(), v2.begin(), v2.end(), v3.begin());
+	for_each(v3.begin(), itEnd, myPrint);
+	cout << endl;
+
+	v3.resize(v1.size() + v2.size());
+	itEnd = set_union(v1.begin(), v1.end(), v2.begin(), v2.end(), v3.begin());
+	for_each(v3.begin(), itEnd, myPrint); 
+	cout << endl;
+
+	v3.resize(max(v1.size(), v2.size()));
+	itEnd = set_difference(v1.begin(), v1.end(), v2.begin(), v2.end(), v3.begin());
+	for_each(v3.begin(), itEnd, myPrint);
+	cout << endl;
+}
+
 int main()
 {
 	/*test();*/
-	test01();
+	/*test01();*/
+	test02();
 
 	return 0;
 }
