be6afc61576af7122d3ac9ecab97dc3466223853
Wilson <93686724+Wilson9098@users.noreply.github.com>
2022-02-05 18:39:19 +1300

    Add files via upload

--- Contest System.cpp
+++ Contest System.cpp
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 
 #include "Contest System.hpp"
+#include <fstream>
+#include <map>
 
 
 void Menu()
@@ -22,6 +24,11 @@ void printNO(const Person& p)
 void printPerson(const Person& p)
 {
 	cout << "Name: " << p.m_Name << "   NO: " << p.m_NO << "   Score: " << p.m_Score << endl;
+}
+
+void getScore(Person& p)
+{
+	p.m_Score = 60 + (rand() % 41);
 }
 
 void Speech()
@@ -52,7 +59,7 @@ void Speech()
 	}
 
 	random_shuffle(person.begin(), person.end());
-	cout << "First trun started: ";
+	cout << "First turn started: ";
 	for_each(person.begin(), person.end(), printNO);
 	cout << endl;
 	system("pause");
@@ -73,9 +80,134 @@ void Speech()
 	for_each(First.begin(), First.end(), printPerson);
 	cout << "Second group scores: " << endl;
 	for_each(Second.begin(), Second.end(), printPerson);
-	
+	cout << endl; 
+	system("pause");
 
+	set<Person, myCompare>::iterator it1 = First.begin();
+	set<Person, myCompare>::iterator it2 = Second.begin();
+	vector<Person> winners;
 
+	winners.push_back(*it1);
+	winners.push_back(*(++it1));
+	winners.push_back(*(++(++it1)));
+
+	winners.push_back(*it2);
+	winners.push_back(*(++it2));
+	winners.push_back(*(++(++it2)));
+
+	sort(winners.begin(), winners.end(), myCompare());
+
+	cout << "winners: " << endl;
+	for_each(winners.begin(), winners.end(), printPerson);
+	cout << endl;
+	system("pause");
+
+	cout << "Second turn started: ";
+	random_shuffle(winners.begin(), winners.end());
+	for_each(winners.begin(), winners.end(), printNO);
+	cout << endl;
+	system("pause");
+
+	for_each(winners.begin(), winners.end(), getScore);
+	for_each(winners.begin(), winners.end(), printPerson);
+	cout << endl;
+	system("pause");
+
+	cout << "winners: " << endl;
+	sort(winners.begin(), winners.end(), myCompare());
+	int i = 0;
+	ofstream Winner;
+	Winner.open("Winner Records.csv", ios::out | ios::app);
+
+	while (i < 3)
+	{
+		Winner << winners[i].m_Name << "," << winners[i].m_NO << "," << winners[i].m_Score << ",";
+		printPerson(winners[i++]);
+	}
+	Winner << endl;
+	Winner.close();
+
+	system("pause");
+	system("cls");
+
+}
+
+void clearRecord()
+{
+	ofstream file;
+	file.open("Winner Records.csv", ios::trunc);
+	if (!file.is_open())
+	{
+		cout << "No Records" << endl;
+	}
+	else
+	{
+		cout << "Cleared" << endl;
+	}
+
+	file.close();
+	system("pause");
+	system("cls");
+}
+
+void loadRecords()
+{
+	ifstream file;
+	file.open("Winner Records.csv", ios::in);
+	if (!file.is_open())
+	{
+		cout << "No Records" << endl;
+		file.close();
+		return;
+	}
+
+	char ch;
+	file >> ch;
+	if (file.eof())
+	{
+		cout << "No Records" << endl;
+		file.close();
+		return;
+	}
+
+	file.putback(ch);
+	int i = 0;
+	map<int, vector<string>> history;
+	string data;
+
+	while (file >> data)
+	{
+		vector<string> v;
+		int p = -1;
+		int start = 0;
+
+		while (1)
+		{
+			p = data.find(",", start);
+			if (p == -1)
+			{
+				break;
+			}
+			else
+			{
+				string tmp = data.substr(start, p - start);
+				v.push_back(tmp);
+				start = p + 1;
+			}
+
+			
+		}
+
+		history.insert(make_pair(++i, v));
+		data = "";
+	}
+
+	file.close();
+
+	for (map<int, vector<string>>::iterator it = history.begin(); it != history.end(); it++)
+	{
+		cout << it->first << "   " << "Winner: " << it->second[0] << "   NO: " << it->second[1] << "   Score: " << it->second[2] << endl;
+	}
 	system("pause");
 	system("cls");
 }
