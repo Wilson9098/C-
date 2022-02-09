45af59866b00d754d4f24efb916a82acb85b5d85
Wilson <93686724+Wilson9098@users.noreply.github.com>
2022-02-09 23:09:15 +1300

    Add files via upload

--- Student.cpp
+++ Student.cpp
@@ -107,7 +107,229 @@ void Student::applyorder()
 void Student::showMyOrder()
 {
 	Order o;
+	if (!o.m_OrderData.size())
+	{
+		cout << "No Order" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	int count = 0;
+	for (int i = 0; i < o.m_OrderData.size(); i++)
+	{
+		if (atoi(o.m_OrderData[i + 1]["ID"].c_str()) == this->m_StudentID)
+		{
+			count++;
+
+			cout << "Date:";
+			if (o.m_OrderData[i + 1]["Date"] == "1")
+			{
+				cout  << "Monday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "2")
+			{
+				cout << "Tuesday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "3")
+			{
+				cout << "Wednesday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "4")
+			{
+				cout << "Thursday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "5")
+			{
+				cout << "Friday" << "   ";
+			}
+
+			cout << "Time:" << (o.m_OrderData[i + 1]["Time"] == "1" ? "Morning" : "Afternoon") << "   "
+				<< "RoomNO:" << o.m_OrderData[i + 1]["RoomNO"] << "   ";
+				
+			if (o.m_OrderData[i + 1]["Status"] == "1")
+			{
+				cout << "Status:" << "Pending" << endl;
+			}
+			else if (o.m_OrderData[i + 1]["Status"] == "2")
+			{
+				cout << "Status:" << "succeeded" << endl;
+			}
+			else if (o.m_OrderData[i + 1]["Status"] == "0")
+			{
+				cout << "Status:" << "Canceled" << endl;
+			}
+			else if (o.m_OrderData[i + 1]["Status"] == "-1")
+			{
+				cout << "Status:" << "Failed" << endl;
+			}
+		}
+
+	}
+	if (!count)
+	{
+		cout << "Your don't have any order yet" << endl;
+	}
 	
+	system("pause");
+	system("cls");
+}
+
+void Student::showAllOrder()
+{
+	Order o;
+	if (!o.m_OrderData.size())
+	{
+		cout << "No Order" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	for (int i = 0; i < o.m_OrderData.size(); i++)
+	{
+			cout << "Date:";
+			if (o.m_OrderData[i + 1]["Date"] == "1")
+			{
+				cout << "Monday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "2")
+			{
+				cout << "Tuesday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "3")
+			{
+				cout << "Wednesday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "4")
+			{
+				cout << "Thursday" << "   ";
+			}
+			if (o.m_OrderData[i + 1]["Date"] == "5")
+			{
+				cout << "Friday" << "   ";
+			}
+
+			cout << "Time:" << (o.m_OrderData[i + 1]["Time"] == "1" ? "Morning" : "Afternoon") << "   "
+				<< "Name:" << o.m_OrderData[i + 1]["Name"] << "   "
+				<< "ID:" << o.m_OrderData[i + 1]["ID"] << "   "
+				<< "RoomNO:" << o.m_OrderData[i + 1]["RoomNO"] << "   ";
+
+			if (o.m_OrderData[i + 1]["Status"] == "1")
+			{
+				cout << "Status:" << "Pending" << endl;
+			}
+			else if (o.m_OrderData[i + 1]["Status"] == "2")
+			{
+				cout << "Status:" << "succeeded" << endl;
+			}
+			else if (o.m_OrderData[i + 1]["Status"] == "0")
+			{
+				cout << "Status:" << "Canceled" << endl;
+			}
+			else if (o.m_OrderData[i + 1]["Status"] == "-1")
+			{
+				cout << "Status:" << "Failed" << endl;
+			}
+
+	}
+
+	system("pause");
+	system("cls");
+}
+
+void Student::cancelOrder()
+{
+	Order o;
+	if (!o.m_OrderData.size())
+	{
+		cout << "No Order" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+
+	int n = 0;
+	vector<int> v;
+	int count = 0;
+	for (int i = 0; i < o.m_OrderData.size(); i++)
+	{
+		if (atoi(o.m_OrderData[i + 1]["ID"].c_str()) == this->m_StudentID)
+		{
+			if (o.m_OrderData[i + 1]["Status"] == "1" || o.m_OrderData[i + 1]["Status"] == "2")
+			{
+				count++;
+				v.push_back(i+1);
+
+				cout << ++n << ": ";
+				cout << "Date:";
+				if (o.m_OrderData[i + 1]["Date"] == "1")
+				{
+					cout << "Monday" << "   ";
+				}
+				if (o.m_OrderData[i + 1]["Date"] == "2")
+				{
+					cout << "Tuesday" << "   ";
+				}
+				if (o.m_OrderData[i + 1]["Date"] == "3")
+				{
+					cout << "Wednesday" << "   ";
+				}
+				if (o.m_OrderData[i + 1]["Date"] == "4")
+				{
+					cout << "Thursday" << "   ";
+				}
+				if (o.m_OrderData[i + 1]["Date"] == "5")
+				{
+					cout << "Friday" << "   ";
+				}
+
+				cout << "Time:" << (o.m_OrderData[i + 1]["Time"] == "1" ? "Morning" : "Afternoon") << "   "
+					<< "RoomNO:" << o.m_OrderData[i + 1]["RoomNO"] << "   ";
+
+				if (o.m_OrderData[i + 1]["Status"] == "1")
+				{
+					cout << "Status:" << "Pending" << endl;
+				}
+				else if (o.m_OrderData[i + 1]["Status"] == "2")
+				{
+					cout << "Status:" << "succeeded" << endl;
+				}
+			}
+		}
+
+	}
+	if (!count)
+	{
+		cout << "Your don't have any order can be canceled yet" << endl;
+	}
+	else
+	{
+		int input = -1;
+		cout << "0. Return" << endl;
+	
+		while(1)
+		{
+			cout << "Please Select: ";
+			cin >> input;
+
+			if (!input)
+			{
+				break;
+			}
+			if (input >= 1 && input <= n)
+			{
+				int i = v[input - 1];
+				o.m_OrderData[i]["Status"] = "0";
+				o.updateOrder();
+				cout << "Canceled" << endl;
+				break;
+			}
+
+			cout << "Try Again" << endl;
+		}
+	}
+
 	system("pause");
 	system("cls");
 }
